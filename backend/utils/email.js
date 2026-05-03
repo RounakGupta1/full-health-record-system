9e70d69d39df577f81b0531a733c315e650154fe
Rounak Gupta <rounak30gupta@gmail.com>
2026-05-03 19:04:24 +0530

    fixed brevo tls

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -2,12 +2,18 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: 587,
   secure: false,
+
+  requireTLS: true,
 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
